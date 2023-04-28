9b1ba0cbfb828dc4ec545968f9a86fcc36b7ccd4
jvanosdol <j.vanosdol99@yahoo.com>
2023-04-27 19:25:47 -0700

    added initial files

--- js/site.js
+++ js/site.js
@@ -1,4 +1,38 @@
-//Call helloWorld
-function helloWorld(){
-    alert("hello world")
+// get the user input string values from the page
+
+function getValue(){
+
+    document.getElementById('alert').classList.add('invisible')
+
+    let userString = document.getElementById('userString').value
+
+    let revString = reverseString(userString)
+    displayString(revString)
+
+}
+
+// reverse the string
+
+function reverseString(userString){
+
+    let revString = []
+
+    // reverse a string using a for loop
+    for( i = userString.length - 1; i >= 0; i-- ){
+        revString += userString[i]
+    }
+
+    return revString
+}
+
+// display the string to the user
+
+function displayString(revString){
+
+    // write to the page
+    document.getElementById('msg').innerHTML = `Your string reversed is: ${revString}`
+
+    //show the alert box
+    document.getElementById('alert').classList.remove('invisible')
+
 }
